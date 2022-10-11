70bcf1472e98ff5d5817bb338ab27e659e6871dc
EC2 Default User <ec2-user@ip-172-31-34-118.ap-northeast-1.compute.internal>
2022-10-11 14:16:41 +0000

    change

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 
 import aws_cdk as cdk
 
-from aurora_bootstraper.aurora_bootstraper_stack import AuroraBootstraperStack
+from aurora_bootstraper.aurora_bootstraper_stack import Aurora
 
 
 app = cdk.App()
-AuroraBootstraperStack(app, "aurora-bootstraper")
+Aurora(app, "Aurora2", env={"region":"ap-northeast-1"}, description="Aurora Cluster",
+  vpc_id    = "vpc-0b8477f7051dc667e",
+  subnet_ids=["subnet-02364a28c0eb3b0a9", "subnet-0da1ebe977855f2ab", "subnet-03791d25a4abf4332"],
+  db_name="gamesp"
+)
 
 app.synth()
